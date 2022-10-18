19b1a3e4a052f88ba18401dd3c43ba70a9995ced
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-19 01:38:59 +0530

    facultyview page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AddstudentComponent } from './addstudent/addstudent.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { StudentviewComponent } from './studentview/studentview.component';
 import { AddfacultyComponent } from './addfaculty/addfaculty.component';
+import { FacultyviewComponent } from './facultyview/facultyview.component';
 
 const appRoutes:Routes=[
   {
@@ -23,6 +24,9 @@ const appRoutes:Routes=[
   },
   {
     path:"addfaculty",component:AddfacultyComponent
+  },
+  {
+    path:"viewfaculty",component:FacultyviewComponent
   }
 ]
 
@@ -33,7 +37,8 @@ const appRoutes:Routes=[
     AddstudentComponent,
     NavbarComponent,
     StudentviewComponent,
-    AddfacultyComponent
+    AddfacultyComponent,
+    FacultyviewComponent
   ],
   imports: [
     BrowserModule,
